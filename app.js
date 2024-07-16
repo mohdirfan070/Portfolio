7417f8434ec14a2af73ee2383d7d9d7ee0dc63b4
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-07-16 12:44:01 +0530

    Text Aniamtion

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const BurgerMenu = document.getElementById('BurgerMenu');
 const BurgerMenuList = document.getElementById('BurgerMenuList');
-BurgerMenu.addEventListener('click',()=>{
+BurgerMenu.addEventListener('click', () => {
     BurgerMenuList.classList.toggle('hidden');
 })
 
-function hideBurgerMenu(){
-    BurgerMenu.click() &&  BurgerMenuList.classList.remove('hidden');
+function hideBurgerMenu() {
+    BurgerMenu.click() && BurgerMenuList.classList.remove('hidden');
 }
+
+
+let heroText = document.getElementById('heroText');
+heroText.innerText = '{';
+const nameArray = ['M', 'o', 'h', 'a', 'm', 'm', 'e', 'd', ' ', 'I', 'r', 'f', 'a', 'n', '}'];
+let i = 0;
+
+ setInterval(() => {
+    heroText.innerText += nameArray[i];
+    i++;
+    if (i > 15) {
+         heroText.innerText='{';
+         return i =0;
+    }
+}, 200)
+
+
+
+ 
+
+
